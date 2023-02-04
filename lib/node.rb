af9c332a8204fe958f587df3d00c5c781c940d0b
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-03 17:34:12 -0700

    Change data to sound

--- lib/node.rb
+++ lib/node.rb
@@ -1,9 +1,9 @@
 class Node 
-  attr_reader :data 
+  attr_reader :sound 
   attr_accessor :next_node
-  def initialize(data, next_node = nil)
+  def initialize(sound, next_node = nil)
     @next_node = next_node
-    @data = data
+    @sound = sound
     
   end
 end
