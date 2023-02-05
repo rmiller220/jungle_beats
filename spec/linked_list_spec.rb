34c1337388c3e8bbd1947c64eedfc7236f26786a
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-04 22:35:55 -0700

    write tests for iteration 2

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -29,9 +29,10 @@ RSpec.describe do
     expect(list.append("boop")).to eq("boop")
     expect(list.head.next_node).to be_an_instance_of(Node)
   end
-
+  
   it 'counts the sounds in a list' do
     list = LinkedList.new
+    require 'pry'; binding.pry
     list.append("doop")
     expect(list.count).to eq(1)
     list.append("boop")
@@ -45,6 +46,69 @@ RSpec.describe do
     list.append("boop")
     expect(list.to_string).to eq("doop boop")
   end
+  it 'puts new sound at the head' do
+    list = LinkedList.new
+    list.append("zap")
+    expect(list.to_string).to eq("zap")
+    list.append("zoo")
+    list.prepend("zip")
+    expect(list.prepend("zip")).to eq("zip")
+    expect(list.to_string).to eq("zip zap zoo")
+    expect(list.count).to eq(3)
+    
+  end
+  
+  xit 'inserts sound at index position' do
+    list = LinkedList.new
+    list.append("zap")
+    list.append("zoo")
+    list.prepend("zip")
+    list.insert(1, "woo")
+    expect(list.insert(1, "woo")).to eq("woo")
+    expect(list.to_string).to eq("zip woo zap zoo")
+  end
+  
+  xit 'finds sound position, returns specified number of sounds' do
+    list = LinkedList.new
+    list.append("zap")
+    list.append("zoo")
+    list.append("zoo")
+    list.prepend("zip")
+    list.insert(1, "woo")
+    expect(list.find(2, 1)).to eq("zap")
+    expect(list.find(1, 3)).to eq("woo zap zoo")
+
+  end
+
+  xit 'checks if a sound is included in list' do
+    list = LinkedList.new
+    list.append("zap")
+    list.append("zoo")
+    list.append("zoo")
+    list.prepend("zip")
+    list.insert(1, "woo")
+    expect(list.to_string).to eq("zip woo zap zoo zoo")
+    expect(list.includes?("zip")).to eq(true)
+    expect(list.includes?("bop")).to eq(false)
+
+  end
+
+  xit 'removes last sound from the list' do
+    list = LinkedList.new
+    list.append("zap")
+    list.append("zoo")
+    list.append("dee")
+    list.prepend("zip")
+    list.insert(1, "woo")
+    expect(list.to_string).to eq("zip woo zap zoo dee")
+    list.pop
+    expect(list.pop).to eq("dee")
+    list.pop
+    expect(list.pop).to eq("zoo")
+    expect(list.to_string).to eq("zip woo zap")
+    
+  end
+
 
 end
 
