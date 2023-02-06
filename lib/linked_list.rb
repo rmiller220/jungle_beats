c8211e31a79af850db04faf26c8425b831b97310
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-05 18:04:38 -0700

    write prepend method and count method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,37 +1,51 @@
 class LinkedList
-  attr_accessor :head, :count #:next_node
-  attr_reader :head, :count #:next_node
+  attr_accessor :head
+  attr_reader :head
   
   def initialize
-    
     @head = nil
-    @count = 0
-                      
+  end
+  
+  def count
+    count = 0
+    current_node = @head
+    until current_node.nil?
+      count += 1
+      current_node = current_node.next_node
+    end
+    count
   end
 
-  def append(sound)
-    @count += 1
+  def prepend(data)
+    current_head = @head
+    @head = Node.new(data)
+    @head.next_node = current_head
+    data
+  end
+  
+
+  def append(data)
     if @head.nil?
-      @head = Node.new(sound)
+      @head = Node.new(data)
     else
       current_node = @head
       until current_node.next_node.nil?
         current_node = current_node.next_node
       end
-        current_node.next_node = Node.new(sound)
+        current_node.next_node = Node.new(data)
     end
-      return sound
+      return data
   end
  
   def to_string
     string = ""
     current_node = @head
     until current_node.nil?
-      string << "#{current_node.sound} "
+      string << "#{current_node.data} "
       current_node = current_node.next_node
     end
     string.strip 
-    #make the list's sounds into a single string
   end
+  
 
 end
