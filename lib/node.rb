629b2cff4b7510686beca875646ffa49963b9379
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-05 18:09:30 -0700

    fix next_node instance variable

--- lib/node.rb
+++ lib/node.rb
@@ -1,9 +1,8 @@
 class Node 
-  attr_reader :sound 
+  attr_reader :data #could be sound
   attr_accessor :next_node
-  def initialize(sound, next_node = nil)
-    @next_node = next_node
-    @sound = sound
-    
+  def initialize(data)
+    @data = data
+    @next_node = nil
   end
 end
