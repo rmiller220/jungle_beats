89f23b17c49322d962aa0fbace01b4439fe61cf2
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-06 18:38:51 -0700

    Write count method

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -8,5 +8,15 @@ class JungleBeat
 
   end
     
+  def append(data)
+    array_of_data = data.split #creates an array that splits a given string into an array of substrings at the spaces
+    array_of_data.each do |data| #
+      list.append(data)
+    end
+    data
+  end
+  def count
+    list.count
+  end
   
 end
