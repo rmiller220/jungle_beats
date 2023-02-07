8ffbfc018b3f52b360bd376a40c97dea5013803b
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-06 18:23:07 -0700

    Write test for append method

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -1,3 +1,4 @@
+require "rspec"
 require "./lib/jungle_beat"
 require "./lib/linked_list"
 require "./lib/node"
@@ -7,7 +8,15 @@ RSpec.describe do
 it 'exists' do
   jb = JungleBeat.new
   list = LinkedList.new
-  expect(jb.list).to be_an_instance_of(JungleBeat)
+   require 'pry'; binding.pry
+  expect(jb.list).to be_an_instance_of(LinkedList)
+  expect(jb.list.head).to eq(nil)
+end
+
+it 'adds to jb' do
+  jb = JungleBeat.new
+  list = LinkedList.new
+  expect(jb.append("zip zap zoo")).to eq("zip zap zoo")
 
 end
 
