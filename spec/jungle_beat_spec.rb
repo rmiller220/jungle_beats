78b5a4baf882c688725bdf5f5eb8a4f0d0f17d75
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-06 18:35:43 -0700

    write test for count method

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -5,20 +5,29 @@ require "./lib/node"
 
 RSpec.describe do
 
-it 'exists' do
-  jb = JungleBeat.new
-  list = LinkedList.new
-   require 'pry'; binding.pry
-  expect(jb.list).to be_an_instance_of(LinkedList)
-  expect(jb.list.head).to eq(nil)
-end
+  it 'exists' do
+    jb = JungleBeat.new
+    list = LinkedList.new
+    require 'pry'; binding.pry
+    expect(jb.list).to be_an_instance_of(LinkedList)
+    expect(jb.list.head).to eq(nil)
+  end
 
-it 'adds to jb' do
-  jb = JungleBeat.new
-  list = LinkedList.new
-  expect(jb.append("zip zap zoo")).to eq("zip zap zoo")
+  it 'adds to jb' do
+    jb = JungleBeat.new
+    list = LinkedList.new
+    expect(jb.append("zip zap zoo")).to eq("zip zap zoo")
+    expect(jb.list.head.data).to eq("zip")
+    expect(jb.list.head.next_node.data).to eq("zap")
+  end
 
-end
+  it 'defines count method' do
+    jb = JungleBeat.new
+    list = LinkedList.new
+    jb.append("zip zap zoo")
+    expect(jb.count).to eq(3)
+
+  end
 
 
 end
