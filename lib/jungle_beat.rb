37f5d80167d917c9723cfa8e9dff85a81a354a53
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-07 14:54:57 -0700

    Change voice, rate, fix spacing

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -2,26 +2,24 @@ class JungleBeat
   attr_accessor :list, :voice, :rate
   
   def initialize()
-
     @list = LinkedList.new
-    @voice = "Boing"
-    @rate = 500
-
   end
     
   def append(data)
-    array_of_data = data.split #creates an array that splits a given string into an array of substrings at the spaces
-    array_of_data.each do |data| #
+    array_of_data = data.split
+    array_of_data.each do |data| 
       list.append(data)
     end
     data
   end
+  
   def count
     list.count
   end
+  
   def play
     sound_list = list.to_string
-    p `say -r 1000 -v Samantha #{sound_list}`
+    p `say -r 1200 -v Samantha #{sound_list}`
   end
   
 end
