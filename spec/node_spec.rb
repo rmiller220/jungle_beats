a4a603ae975fd1d50e96ec43bbdad9044109c2f6
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-07 14:43:22 -0700

    more tests for branch with notes

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -8,13 +8,11 @@ RSpec.describe Node do
     node = Node.new("plop")
     expect(node).to be_an_instance_of(Node)
     expect(node.data).to eq("plop")
-  # require 'pry'; binding.pry
   end
 
   it 'finds next node' do
     node = Node.new("plop")
     expect(node.next_node).to eq(nil)
-    # require 'pry'; binding.pry
   end
 
 end
